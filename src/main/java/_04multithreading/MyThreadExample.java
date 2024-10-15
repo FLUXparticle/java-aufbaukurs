ef56bc9c01b44a9e9e86b37e30f24cd78f8c8bd7
Sven Reinck <sreinck@fluxparticle.de>
2024-10-15 12:07:22 +0200

    Daemon-Thread

--- src/main/java/_04multithreading/MyThreadExample.java
+++ src/main/java/_04multithreading/MyThreadExample.java
@@ -5,13 +5,28 @@ public class MyThreadExample {
     public static void main(String[] args) {
         // Thread mit Methodenreferenz erzeugen
         Thread thread = new Thread(MyThreadExample::myTask);
+        thread.setDaemon(true);
         
         // Thread starten
         thread.start();
+
+        System.out.println("Thread is running: " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Thread is running: " + Thread.currentThread().getName());
     }
 
     // Aufgabe, die der Thread ausführt
     public static void myTask() {
+        System.out.println("Thread is running: " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("Thread is running: " + Thread.currentThread().getName());
     }
 
